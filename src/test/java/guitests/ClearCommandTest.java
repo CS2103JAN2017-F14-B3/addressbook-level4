8815f36c5af82adbd0256e8477a7967251fc37b7
LESLIE HO ZONG HONG <e0011875@u.nus.edu.sg>
2017-04-07 17:06:30 +0800

    1) Remove comment and incoporate milliseconds into the constant name in Clock.java 2) Rename formattedDate() to getCurrentFormattedDate() in Clock.java 3) Change MESSAGE_SUCCESS from "iManager has been cleared!" to "Tasklist has been cleared" 5) Added space between example string COMMAND_WORD and TYPE_DONE in ViewCommand.java

--- src/test/java/guitests/ClearCommandTest.java
+++ src/test/java/guitests/ClearCommandTest.java
@@ -26,6 +26,6 @@ public class ClearCommandTest extends TaskListGuiTest {
     private void assertClearCommandSuccess() {
         commandBox.runCommand("clear");
         assertListSize(0);
-        assertResultMessage("iManager has been cleared!");
+        assertResultMessage("Tasklist has been cleared!");
     }
 }
